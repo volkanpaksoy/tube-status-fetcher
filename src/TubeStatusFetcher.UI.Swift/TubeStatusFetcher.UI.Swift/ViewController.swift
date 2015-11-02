f52996863af1bbf365e813e074e850892193287b
VOLKAN PAKSOY <volkan@VOLKANs-MBP.home>
2015-11-02 19:34:34 +0000

    Added empty Swift project

---
+++ src/TubeStatusFetcher.UI.Swift/TubeStatusFetcher.UI.Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TubeStatusFetcher.UI.Swift
+//
+//  Created by VOLKAN PAKSOY on 02/11/2015.
+//  Copyright © 2015 VOLKAN PAKSOY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
