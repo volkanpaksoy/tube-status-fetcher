5e7f0e9f8c8b89d2899a3d202e4962f9f5aae14c
Volkan Paksoy <volkan@VOLKANs-MBP.home>
2015-11-07 11:56:52 +0000

    Added Xamarin project

---
+++ src/TubeStatusFetcher.UI.Swift/TubeStatusFetcher.UI.Swift/Model/TFLConfig.swift
@@ -0,0 +1,13 @@
+//
+//  TFLConfig.swift
+//  TubeStatusFetcher.UI.Swift
+//
+//  Created by VOLKAN PAKSOY on 07/11/2015.
+//  Copyright © 2015 VOLKAN PAKSOY. All rights reserved.
+//
+
+import UIKit
+
+class TFLConfig: NSObject {
+
+}
